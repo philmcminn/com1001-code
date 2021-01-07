00b9054503927b12a9fc7a65c872dc06aec0acdc
Phil McMinn <phil@mcminn.io>
2021-01-07 13:49:56 +0000

    Adding unit testing directory

---
+++ unit_testing/buggy_string_comparison.rb
@@ -0,0 +1,11 @@
+def string_comparison(str1, str2)
+
+    for i in 1..[str1.length, str2.length].max-1 
+        return i if str1[i] != str2[i]
+    end
+
+    -1
+end
+
+puts string_comparison("aardvark", "aargh")
+puts string_comparison("sought", "bought")
