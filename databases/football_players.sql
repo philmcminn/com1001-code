631a0353f332927861c29ffd59be5c1d6e9335d2
Phil McMinn <phil@mcminn.io>
2021-01-12 10:30:27 +0000

    Adding database example SQL file

---
+++ databases/football_players.sql
@@ -0,0 +1,19 @@
+CREATE TABLE players (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    surname TEXT,
+    gender TEXT,
+    date_of_birth TEXT,
+    country TEXT
+);
+
+INSERT INTO players (1, "Dominic", "Calvert-Lewin", "M", "1997-03-16", "England");
+INSERT INTO players (2, "Sam", "Kerr", "F", "1993-09-10", "Australia");
+INSERT INTO players (3, "Harry", "Kane", "M", "1993-07-28", "England");
+INSERT INTO players (4, "Rose", "Lavelle", "F", "1995-05-14", "USA");
+INSERT INTO players (5, "Son", "Heung-min", "M", "1992-07-08", "South Korea");
+INSERT INTO players (6, "Pernille", "Harder", "F", "1992-11-15", "Denmark");
+INSERT INTO players (7, "Bruno", "Fernandes", "M", "1994-09-08", "Portugal");
+INSERT INTO players (8, "Hayley", "Raso", "F", "1994-09-05", "Australia");
+INSERT INTO players (9, "Kevin", "De Bruyne", "M", "1991-06-28", "Belgium");
+INSERT INTO players (10, "Vivianne", "Miedema", "F", "1996-07-15", "Netherlands");
