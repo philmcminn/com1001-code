92941d1a07e9b4bb4f519459233505ec0e1d715d
Phil McMinn <phil@mcminn.io>
2021-01-13 13:57:12 +0000

    Updating greedy wildcard example

--- routes/greedy_wildcard.rb
+++ routes/greedy_wildcard.rb
@@ -2,7 +2,7 @@ require "sinatra"
 set :bind, "0.0.0.0"
 
 get "/*" do
-  "I am the god route. Nothing shall escape me"
+  "I am the god of all routes. Nothing shall get past me"
 end
 
 get "/introverted-route" do
