e0b4ce95f98a6b00fe27d31ed1ca3087e8457727
Phil McMinn <phil@mcminn.io>
2021-01-19 15:44:53 +0000

    Adding new database examples

---
+++ databases/sequel_select.rb
@@ -0,0 +1,19 @@
+require "logger"
+require "sequel"
+
+DB = Sequel.sqlite("football_players.sqlite3",
+                   logger: Logger.new("db.log"))
+
+puts "Please enter a club name:"
+supplied_club = gets.chomp
+
+dataset = DB[:players].where(club: supplied_club)
+num_rows = dataset.count
+
+if num_rows.zero?
+  puts "Sorry there are no players for that club."
+else
+  dataset.each do |record|
+    puts "* #{record[:first_name]} #{record[:surname]}"
+  end
+end
