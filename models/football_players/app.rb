cdb163f799129d28c443c729ea8eefe13eb1fcab
Phil McMinn <phil@mcminn.io>
2021-01-22 14:42:01 +0000

    Updates to the models/football_players app

--- models/football_players/app.rb
+++ models/football_players/app.rb
@@ -7,6 +7,9 @@ require "sinatra"
 # For Codio
 set :bind, '0.0.0.0'
 
+# So we can escape HTML special characters in the view
+include ERB::Util
+
 # Database
 db_name = ENV.fetch("APP_ENV", "production")
 DB = Sequel.sqlite("db/#{db_name}.sqlite3",
