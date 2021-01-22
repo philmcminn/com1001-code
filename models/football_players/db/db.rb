1db977f027e99ce30d3f52bbde0342e9b2ca9e56
Phil McMinn <phil@mcminn.io>
2021-01-22 18:32:05 +0000

    Correcting Rubocop offences in models

--- models/football_players/db/db.rb
+++ models/football_players/db/db.rb
@@ -2,10 +2,10 @@ require "logger"
 require "sequel"
 
 unless defined?(DB)
-    type = ENV.fetch("APP_ENV", "production")
-    path = File.dirname(__FILE__)
-    db = "#{path}/#{type}"    
+  type = ENV.fetch("APP_ENV", "production")
+  path = File.dirname(__FILE__)
+  db = "#{path}/#{type}"
 
-    DB = Sequel.sqlite("#{db}.sqlite3", 
-                       logger: Logger.new("#{db}.log"))
+  DB = Sequel.sqlite("#{db}.sqlite3",
+                     logger: Logger.new("#{db}.log"))
 end
