1c59c0584635bbc3db0289cfd02c157a116c55b0
Phil McMinn <phil@mcminn.io>
2021-01-26 10:49:41 +0000

    Updates to specs in forms/simple_forms

---
+++ forms/simple_forms/spec/features/simple_forms_spec.rb
@@ -0,0 +1,38 @@
+require "capybara/rspec"
+require "rack/test"
+require "rspec"
+
+require_relative "../../app"
+
+Capybara.app = Sinatra::Application
+
+RSpec.describe "Simple Form Examples" do
+  include Capybara::DSL
+
+  describe "Simple Form Example" do    
+    it "submits some data" do
+      visit "/simple-form"
+      fill_in "text_field", with: "Some Text"
+      click_button "Submit"
+      expect(page).to have_content "You submitted the text: Some Text"
+    end
+  end
+
+  describe "Get Form Example" do    
+    it "submits some data" do
+      visit "/get-form"
+      fill_in "text_field", with: "Some Text"
+      click_button "Submit"
+      expect(page).to have_content "You submitted the text: Some Text"
+    end
+  end
+
+  describe "Post Form Example" do    
+    it "submits some data" do
+      visit "/post-form"
+      fill_in "text_field", with: "Some Text"
+      click_button "Submit"
+      expect(page).to have_content "You submitted the text: Some Text"
+    end
+  end
+end
