6aed7a8920456cc3952fd02138b9397b672ab194
Phil McMinn <p.mcminn@sheffield.ac.uk>
2021-02-23 16:03:19 +0000

    Updating hello world example

--- basics/hello_world.rb
+++ basics/hello_world.rb
@@ -4,3 +4,17 @@ set :bind, "0.0.0.0"
 get "/hello-world" do
   "Hello, World!"
 end
+
+# These lines of below just print the message for the URL that you need for Codio.
+# They are not needed for Sinatra applications in general.
+#
+if ENV["USER"] == "codio"
+  box_name = ENV["CODIO_HOSTNAME"]
+  port = settings.port
+  base_url = "https://#{box_name}-#{port}.codio.io"
+  route = "/hello-world"
+  url = "#{base_url}#{route}"
+  message = "  CLICK THIS URL TO ACCESS THIS EXAMPLE: #{url}  "
+  stars = "*" * message.length
+  puts "\n#{stars}\n#{message}\n#{stars}\n\n"
+end
