de27c214ca4107d6ed0635ae01439651e6797f0a
Phil McMinn <p.mcminn@sheffield.ac.uk>
2021-03-03 14:53:38 +0000

    Fixes

--- basics/hello_world.rb
+++ basics/hello_world.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 set :bind, "0.0.0.0"
 
-get "/" do
+get "/hello-world" do
   "Hello, World!"
 end
 
--- forms/football_players/spec/unit/delete_spec.rb
+++ forms/football_players/spec/unit/delete_spec.rb
@@ -2,7 +2,7 @@ require_relative "../spec_helper"
 
 RSpec.describe "Football Players App" do
   describe "POST/delete" do
-    it "does not allow deletion of a invalid player" do
+    it "does not allow deletion of an invalid player" do
       post "/delete", "id" => "-100"
       expect(last_response.body).to include("Unknown player")
     end
