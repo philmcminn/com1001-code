d3f261e6f350e12a9ce7ddff26fc737f87988577
Phil McMinn <p.mcminn@sheffield.ac.uk>
2021-03-03 14:51:04 +0000

    Re-adding database file to orm directory

--- basics/hello_world.rb
+++ basics/hello_world.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 set :bind, "0.0.0.0"
 
-get "/hello-world" do
+get "/" do
   "Hello, World!"
 end
 
--- models/football_players/models/player.rb
+++ models/football_players/models/player.rb
@@ -2,6 +2,7 @@ require "time_difference"
 
 # A player record from the database
 class Player < Sequel::Model
+
   def name
     "#{first_name} #{surname}"
   end
@@ -9,5 +10,5 @@ class Player < Sequel::Model
   def age(at_date = Date.today)
     dob = Date.strptime(date_of_birth, "%Y-%m-%d")
     TimeDifference.between(dob, at_date).in_years.floor
-  end
+  end   
 end
